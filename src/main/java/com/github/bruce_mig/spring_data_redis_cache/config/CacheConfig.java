f84727af755b4f2855588773fa08ac3982af9555
bruce-mig <brucetmig@icloud.com>
2024-11-03 03:17:08 +0200

    using dynamic TTL

--- src/main/java/com/github/bruce_mig/spring_data_redis_cache/config/CacheConfig.java
+++ src/main/java/com/github/bruce_mig/spring_data_redis_cache/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.github.bruce_mig.spring_data_redis_cache.config;
 
+import com.github.bruce_mig.spring_data_redis_cache.util.CustomTtlFunction;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -7,9 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
-import java.time.Duration;
 import java.util.Collections;
 
 @Configuration
@@ -18,16 +17,13 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
-
         RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(1)); // Set the TTL to 1 minute
+                .disableCachingNullValues()
+                .entryTtl(CustomTtlFunction.INSTANCE); // Set the custom TTL function globally
 
         return RedisCacheManager.builder(redisConnectionFactory)
                 .cacheDefaults(cacheConfiguration)
                 .transactionAware()
-                .withInitialCacheConfigurations(Collections.singletonMap("predefined",
-                        RedisCacheConfiguration.defaultCacheConfig().disableCachingNullValues()))
                 .build();
     }
 }
