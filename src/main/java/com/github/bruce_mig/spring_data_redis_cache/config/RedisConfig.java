d7dc9c04636f1e8c561ae25a5aa382be61333440
bruce-mig <brucetmig@icloud.com>
2024-12-18 09:13:45 +0200

    upgraded deprecated redis config

--- src/main/java/com/github/bruce_mig/spring_data_redis_cache/config/RedisConfig.java
+++ src/main/java/com/github/bruce_mig/spring_data_redis_cache/config/RedisConfig.java
@@ -12,13 +12,13 @@ import org.springframework.data.redis.connection.lettuce.LettuceClientConfigurat
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("${spring.redis.username}")
+    @Value("${spring.data.redis.username}")
     private String username;
 
-    @Value("${spring.redis.password}")
+    @Value("${spring.data.redis.password}")
     private String password;
 
     @Bean
